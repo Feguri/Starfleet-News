a942601d1948b30bcf5599b085f60805fc998c9f
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2021-10-27 16:35:29 -0400

    Add files via upload

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -1,11 +1,16 @@
 let bts = document.getElementsByClassName('join');
 function redirect() {
-    window.open('https://feguri.github.io/Starfleet-News/QUARK');
+    window.open('/C:/Users/Acer/vscode/WebDev/starfleetNews/QUARK.html');
 }
+function redirectMain() {
+    location.reload();
+}
+
 function redirectArticle1() {
-    window.open('https://feguri.github.io/Starfleet-News/article-1')
+    window.open('/C:/Users/Acer/vscode/WebDev/starfleetNews/article-1.html')
 }
 for (let bt of bts) {
     bt.addEventListener('click', redirect);
 }
 document.getElementById('article1').addEventListener('click', redirectArticle1);
+document.getElementById('starfleet').addEventListener('click', redirectMain);
